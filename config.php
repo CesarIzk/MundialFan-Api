e64cab468bb059ce3774050795fa6f382ae108e8
CesarIzk <125629498+CesarIzk@users.noreply.github.com>
2026-04-24 21:09:16 -0600

    try fix deploy config

--- config.php
+++ config.php
@@ -1,11 +1,34 @@
 <?php
 
+// Railway usa MYSQL_URL en lugar de DATABASE_URL
+$url = getenv('MYSQL_URL') ?: getenv('DATABASE_URL');
+
+// Configuración local por defecto
+if ($url === false) {
+    return [
+        'database' => [
+            'host' => 'localhost',
+            'port' => 3306,
+            'user' => 'root',
+            'password' => 'EwebDfGLTEAdcqOwJpMbELbIlyXabWhX',
+            'dbname' => 'railway', // 👈 Cambia esto al nombre de tu BD local
+            'charset' => 'utf8mb4'
+        ]
+    ];
+}
+
+// Descompone la URL de Railway
+$parts = parse_url($url);
+
 return [
-    "database" => [
-        "host" => "centerbeam.proxy.rlwy.net",
-        "user" => "root",
-        "password" => "EwebDfGLTEAdcqOwJpMbELbIlyXabWhX",
-        "dbname" => "railway",
-        "port" => "35011",
-    ],
+    'database' => [
+        'host' => $parts['host'],
+        'port' => $parts['port'] ?? 3306,
+        'user' => $parts['user'],
+        'password' => $parts['pass'],
+        'dbname' => ltrim($parts['path'], '/'),
+        'charset' => 'utf8mb4'
+    ]
 ];
+
+
